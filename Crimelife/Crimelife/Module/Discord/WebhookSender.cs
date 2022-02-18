22d3cc63e106ebd69e832967fe542d88c88e7045
Rojas1337 <95372712+Rojas1337@users.noreply.github.com>
2022-02-18 19:12:30 +0100

    script upload

---
+++ Crimelife/Crimelife/Module/Discord/WebhookSender.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Crimelife
+{
+    class WebhookSender : Crimelife.Module.Module<WebhookSender>
+    {
+        public static async void SendMessage(string title, string msg, string webhook, string type)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                string[] strArray = new string[20];
+                strArray[0] = "{\"username\":\"Galagtic-Crimelife\",\"avatar_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\",\"content\":\"\",\"embeds\":[{\"author\":{\"name\":\"Galagtic-Crimelife\",\"url\":\"https://discord.gg/435\",\"icon_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"title\":\"" + type + "\",\"thumbnail\":{\"url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"url\":\"https://discord.gg/435\",\"description\":\"Es wurde am **";
+                int num = now.Day;
+                strArray[1] = num.ToString();
+                strArray[2] = ".";
+                num = now.Month;
+                strArray[3] = num.ToString();
+                strArray[4] = ".";
+                num = now.Year;
+                strArray[5] = num.ToString();
+                strArray[6] = " | ";
+                num = now.Hour;
+                strArray[7] = num.ToString();
+                strArray[8] = ":";
+                num = now.Minute;
+                strArray[9] = num.ToString();
+                strArray[10] = "** ein " + type + " ausgelöst.\",\"color\":1127128,\"fields\":[{\"name\":\"";
+                strArray[11] = title;
+                strArray[12] = "\",\"value\":\"";
+                strArray[13] = msg;
+                strArray[14] = "\",\"inline\":true}],\"footer\":{\"text\":\"Galagtic-Crimelife | " + type + " Bot (c) 2021\"}}]}";
+                string stringPayload = string.Concat(strArray);
+                StringContent httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(webhook, (HttpContent)httpContent);
+                }
+                stringPayload = (string)null;
+                httpContent = (StringContent)null;
+            }
+            catch (Exception ex)
+            {
+                Logger.Print("[EXCEPTION SendMessage] " + ex.Message);
+                Logger.Print("[EXCEPTION SendMessage] " + ex.StackTrace);
+            }
+        }
+
+        public static async void SendMessage423(string title, string msg, string webhook, string type)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                string[] strArray = new string[20];
+                strArray[0] = "{\"username\":\"Galagtic-Crimelife\",\"avatar_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\",\"content\":\"\",\"embeds\":[{\"author\":{\"name\":\"Galagtic-Crimelife Crimelife\",\"url\":\"https://discord.gg/Galagtic-Crimelifecrimelife\",\"icon_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"title\":\"" + type + "\",\"thumbnail\":{\"url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"url\":\"https://discord.gg/Galagtic-Crimelifecrimelife\",\"description\":\"Es wurde am **";
+                int num = now.Day;
+                strArray[1] = num.ToString();
+                strArray[2] = ".";
+                num = now.Month;
+                strArray[3] = num.ToString();
+                strArray[4] = ".";
+                num = now.Year;
+                strArray[5] = num.ToString();
+                strArray[6] = " | ";
+                num = now.Hour;
+                strArray[7] = num.ToString();
+                strArray[8] = ":";
+                num = now.Minute;
+                strArray[9] = num.ToString();
+                strArray[10] = "** ein " + type + " ausgelöst.\",\"color\":1127128,\"fields\":[{\"name\":\"";
+                strArray[11] = title;
+                strArray[12] = "\",\"value\":\"";
+                strArray[13] = msg;
+                strArray[14] = "\",\"inline\":true}],\"footer\":{\"text\":\"Galagtic-Crimelife Crimelife | " + type + " Bot (c) 2021\"}}]}";
+                string stringPayload = string.Concat(strArray);
+                StringContent httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(webhook, (HttpContent)httpContent);
+                }
+                stringPayload = (string)null;
+                httpContent = (StringContent)null;
+            }
+            catch (Exception ex)
+            {
+                Logger.Print("[EXCEPTION SendMessage] " + ex.Message);
+                Logger.Print("[EXCEPTION SendMessage] " + ex.StackTrace);
+            }
+        }
+
+        public static async void SendMessage321(string title, string msg, string webhook, string type)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                string[] strArray = new string[20];
+                strArray[0] = "{\"username\":\"Galagtic-Crimelife\",\"avatar_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\",\"content\":\"\",\"embeds\":[{\"author\":{\"name\":\"Galagtic-Crimelife Crimelife\",\"url\":\"https://discord.gg/Galagtic-Crimelifecrimelife\",\"icon_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"title\":\"" + type + "\",\"thumbnail\":{\"url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"url\":\"https://discord.gg/Galagtic-Crimelifecrimelife\",\"description\":\"Es wurde am **";
+                int num = now.Day;
+                strArray[1] = num.ToString();
+                strArray[2] = ".";
+                num = now.Month;
+                strArray[3] = num.ToString();
+                strArray[4] = ".";
+                num = now.Year;
+                strArray[5] = num.ToString();
+                strArray[6] = " | ";
+                num = now.Hour;
+                strArray[7] = num.ToString();
+                strArray[8] = ":";
+                num = now.Minute;
+                strArray[9] = num.ToString();
+                strArray[10] = "** ein " + type + " ausgelöst.\",\"color\":1127128,\"fields\":[{\"name\":\"";
+                strArray[11] = title;
+                strArray[12] = "\",\"value\":\"";
+                strArray[13] = msg;
+                strArray[14] = "\",\"inline\":true}],\"footer\":{\"text\":\"Galagtic-Crimelife Crimelife | " + type + " Bot (c) 2021\"}}]}";
+                string stringPayload = string.Concat(strArray);
+                StringContent httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(webhook, (HttpContent)httpContent);
+                }
+                stringPayload = (string)null;
+                httpContent = (StringContent)null;
+            }
+            catch (Exception ex)
+            {
+                Logger.Print("[EXCEPTION SendMessage] " + ex.Message);
+                Logger.Print("[EXCEPTION SendMessage] " + ex.StackTrace);
+            }
+        }
+
+        public static async void SendMessage123(string title, string msg, string webhook, string type)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                string[] strArray = new string[20];
+                strArray[0] = "{\"username\":\"Galagtic-Crimelife\",\"avatar_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\",\"content\":\"\",\"embeds\":[{\"author\":{\"name\":\"Galagtic-Crimelife Crimelife\",\"url\":\"https://discord.gg/Galagtic-Crimelifecrimelife\",\"icon_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"title\":\"" + type + "\",\"thumbnail\":{\"url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"url\":\"https://discord.gg/Galagtic-Crimelifecrimelife\",\"description\":\"Es wurde am **";
+                int num = now.Day;
+                strArray[1] = num.ToString();
+                strArray[2] = ".";
+                num = now.Month;
+                strArray[3] = num.ToString();
+                strArray[4] = ".";
+                num = now.Year;
+                strArray[5] = num.ToString();
+                strArray[6] = " | ";
+                num = now.Hour;
+                strArray[7] = num.ToString();
+                strArray[8] = ":";
+                num = now.Minute;
+                strArray[9] = num.ToString();
+                strArray[10] = "** ein " + type + " ausgelöst.\",\"color\":1127128,\"fields\":[{\"name\":\"";
+                strArray[11] = title;
+                strArray[12] = "\",\"value\":\"";
+                strArray[13] = msg;
+                strArray[14] = "\",\"inline\":true}],\"footer\":{\"text\":\"Galagtic-Crimelife Crimelife | " + type + " Bot (c) 2021\"}}]}";
+                string stringPayload = string.Concat(strArray);
+                StringContent httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(webhook, (HttpContent)httpContent);
+                }
+                stringPayload = (string)null;
+                httpContent = (StringContent)null;
+            }
+            catch (Exception ex)
+            {
+                Logger.Print("[EXCEPTION SendMessage] " + ex.Message);
+                Logger.Print("[EXCEPTION SendMessage] " + ex.StackTrace);
+            }
+        }
+
+        public static async void SendMessage534(string title, string msg, string webhook, string type)
+        {
+            try
+            {
+                DateTime now = DateTime.Now;
+                string[] strArray = new string[20];
+                strArray[0] = "{\"username\":\"Galagtic-Crimelife\",\"avatar_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\",\"content\":\"\",\"embeds\":[{\"author\":{\"name\":\"Galagtic-Crimelife Crimelife\",\"url\":\"https://discord.gg/Galagtic-Crimelifecrimelife\",\"icon_url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"title\":\"" + type + "\",\"thumbnail\":{\"url\":\"https://i.ytimg.com/vi/uNlVJbYLuJQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqj12lJckGH09F9GgzyNAl1802XQ\"},\"url\":\"https://discord.gg/Galagtic-Crimelifecrimelife\",\"description\":\"Es wurde am **";
+                int num = now.Day;
+                strArray[1] = num.ToString();
+                strArray[2] = ".";
+                num = now.Month;
+                strArray[3] = num.ToString();
+                strArray[4] = ".";
+                num = now.Year;
+                strArray[5] = num.ToString();
+                strArray[6] = " | ";
+                num = now.Hour;
+                strArray[7] = num.ToString();
+                strArray[8] = ":";
+                num = now.Minute;
+                strArray[9] = num.ToString();
+                strArray[10] = "** ein " + type + " ausgelöst.\",\"color\":1127128,\"fields\":[{\"name\":\"";
+                strArray[11] = title;
+                strArray[12] = "\",\"value\":\"";
+                strArray[13] = msg;
+                strArray[14] = "\",\"inline\":true}],\"footer\":{\"text\":\"Galagtic-Crimelife Crimelife | " + type + " Bot (c) 2021\"}}]}";
+                string stringPayload = string.Concat(strArray);
+                StringContent httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpResponseMessage httpResponseMessage = await httpClient.PostAsync(webhook, (HttpContent)httpContent);
+                }
+                stringPayload = (string)null;
+                httpContent = (StringContent)null;
+            }
+            catch (Exception ex)
+            {
+                Logger.Print("[EXCEPTION SendMessage] " + ex.Message);
+                Logger.Print("[EXCEPTION SendMessage] " + ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ Crimelife/Crimelife/Module/Discord/Webhooks.cs
@@ -0,0 +1,25 @@
+﻿namespace Crimelife
+{
+    class Webhooks
+    {
+        public static string joinlogs = "https://discord.com/api/webhooks/938950116394549329/qx7svfFrdLkKdxGIS4e-tLu59BO7_Zzj2QJWrHcdSD0LuEI7h-pjS2U8eVoNbgX2dlCA";
+        public static string itemlogs = "https://discord.com/api/webhooks/938950022198882374/ChVqD6-l9u7xUL4NY_UBJO5sHnOLrbWfSVStLuMWViQIT7T1PiWMEJZF6YYrHrmmvUqd";
+        public static string kofferaumlogs = "https://discord.com/api/webhooks/938950231838576650/pz8Okd9mbyH3zshrkCNyvFvdGPKt-a1-hiLvBVgsxHntr3J6UQ4ow0lrLHOTVm-nzgTV";
+        public static string fraklagerlogs = "https://discord.com/api/webhooks/938950322196459600/yE3uDyDa0EhwUmTF6_1oK_InQpA2m0SfiAjmfHlBOlXRGTuNimU8yGDVjgPgSZ_COmw8";
+        public static string geldlogs = "https://discord.com/api/webhooks/938950430094925834/4BqTJxJFLbSXNdSbHTD4Jw7Ug57xNLntrE81eznC4meEmmrAthAT4wFG7r-xnJIsfQJ7";
+        public static string adminlogs = "https://discord.com/api/webhooks/938950512911450122/fYKc5UkmMPTcpv-vy5em3AF12SzF0Vb4tv74N25qARvzzs9HncyeJwGiUd-0e_Rxq-f2";
+        public static string banlogs = "https://discord.com/api/webhooks/938950673314230284/E3O7BpENQMwdad_vow_3-bHWvfbYrfTF7D3FKEr750WT5gThvgoljrWztgnAdSWGzrgD";
+        public static string kicklogs = "https://discord.com/api/webhooks/938950587993702430/22Osy50wY_s9aWWO5jxmf_uJDbCYZ9UFUi4fR1k9LA9340PwlY-IPjQxvcp-Gs_ueW09";
+        public static string revivelogs = "https://discord.com/api/webhooks/938950793107755039/GEMOH-ZYkKNSmFAlAkwtLoph-aKRZ6FhwptKtFgSIh9PQ-7EOcs3qqaKWfONmgIitXvC";
+        public static string shoplogs = "https://discord.com/api/webhooks/940390307667800075/tDaK4DUQHvpnj0uUfBseLgQ4QZW1GMjktdceutTESoIiRE4uHR-UT_PiMaJ-hvt-4-ed";
+        public static string autokauflogs = "https://discord.com/api/webhooks/940390415310393344/lrLf42NHqn7LOx1j8jf0yQrzqrcav2Dd9Ia97gYWJH7SUeQO4AlnejdEzjLyPUxH4fUK";
+        public static string killWebhook = "https://discord.com/api/webhooks/938949859526983721/ngDfJwbleM1Vx2n0V_D9hXJNH8FmHYqEEygC-_oe0Fo_T7or-mS87PryXa-XUoT7InkB";
+        public static string supportlogs = "https://discord.com/api/webhooks/940390722526396476/89IYn2nj_HP-Zr4ooaYkIgsflYBgTFYo60_uJ7aM8lYUF-A4snQkVMXzG0sT2Qv64-l2";
+        public static string hauslagerlogs = "https://discord.com/api/webhooks/940390832102572143/WKwvFkj6ZGwABIrH_EG5PpWBcSQN9Al462I0vRpQ6RN1LUFD9CaFxCpnCMfb1JBBzvGV";
+        public static string commandlogs = "https://discord.com/api/webhooks/940390907440685126/-iB7qwvfK0dRNsiY0bMZ4ZHuiGUgMnf5mvJ0R3-upY5Qm6MmqEZLSlMix-ELBaXu9Tld";
+        public static string aclogs = "https://discord.com/api/webhooks/940390977913380915/Tdjwd8NTqKHvmU22lgl3cOmjkMTbbceI_ijyPYtW1KfxipVdF0cuGfPEnvcTJgs4BZqt";
+        public static string unbanlogs = "https://discord.com/api/webhooks/940391049661153341/nBeWSAYyE62uwqEbN2vJqKHJUtubRPc7fJNq-XGNIL8HYEe960VCdufYbaMijFWM4vGZ";
+        public static string rathauslogs = "https://discord.com/api/webhooks/940391115138420786/5q3aAh611KGsteJJPjgI1pHvqk5WojCdqHNGt_aijl7Psrq7bUs9HmrlKSc7gyObQ_ki";
+        public static string disconnectlogs = "https://discord.com/api/webhooks/940391189100777512/brRQlPDhS7h4ffdNAjGvdpdbMIQFznCoRlIZBvtHhH34nJHOHrKSKM98lWgamN_m2D1V";
+    }
+}
